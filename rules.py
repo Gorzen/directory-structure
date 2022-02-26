0d8ed38f8daf1b7e2eb9e57cffe47757a2d92b39
Lucien Iseli <lucien.iseli93@gmail.com>
2022-02-26 14:04:48 +0100

    Clean rules.py

--- rules.py
+++ rules.py
@@ -1,23 +1,55 @@
-class NoUnknownDirectories:
+# -*- coding: utf-8 -*-
+
+"""Defines all the different rules for the directory structure."""
+
+from abc import ABC, abstractmethod
+
+class Rule(ABC):
+    """A rule for the directory structure"""
+
+    def __repr__(self):
+        "Rule()"
+
+    @abstractmethod
+    def check(self, path):
+        """Check that rule is valid for the directory located at path"""
+        return NotImplemented
+
+
+class NoUnknownDirectories(Rule):
+    """No unknown (not declared in the directory structure) directories allowed rule."""
+
     key = 'noUnknownDirectories'
 
-    def check(path):
+    def __repr__(self):
+        "NoUnknownDirectories()"
+
+    def check(self, path):
         return ''
 
 
-class NoVisibleFiles:
+class NoVisibleFiles(Rule):
+    """No visible files allowed rule."""
+
     key = 'noVisibleFiles'
 
-    def check(path):
+    def __repr__(self):
+        "NoVisibleFiles()"
+
+    def check(self, path):
         return ''
 
 
-class NoHiddenFiles:
+class NoHiddenFiles(Rule):
+    """No hidden files allowed rule."""
+
     key = 'noHiddenFiles'
 
-    def check(path):
+    def __repr__(self):
+        "NoHiddenFiles()"
+
+    def check(self, path):
         return ''
 
 
 allRules = [NoUnknownDirectories, NoVisibleFiles, NoHiddenFiles]
-
