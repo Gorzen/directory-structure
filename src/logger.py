9920f138409fccc9099517cc31021c65d3eaf72b
Lucien Iseli <lucien.iseli93@gmail.com>
2022-02-26 14:48:55 +0100

    Add type hints for logger

--- src/logger.py
+++ src/logger.py
@@ -6,13 +6,13 @@
 class Logger:
     """Class used for logging messages."""
 
-    def __init__(self, is_verbose):
+    def __init__(self, is_verbose: bool) -> None:
         self.is_verbose = is_verbose
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Logger(is_verbose={self.is_verbose})"
 
-    def verbose(self, msg):
+    def verbose(self, msg: str) -> None:
         """Prints msg if logger is verbose."""
         if self.is_verbose:
             print(msg)
